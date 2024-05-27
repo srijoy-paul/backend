e475c0d5c535d6458bc4aed25fbf691849f9f529
Srijoy Paul <srijoykumar1507@gmail.com>
2024-05-27 21:32:46 +0530

    GetAllReviews Api(sortoptions,username),rating,comments), for a restaurant

--- Routes/ReviewsRoutes.ts
+++ Routes/ReviewsRoutes.ts
@@ -10,59 +10,94 @@ router.post(
   jwtCheck,
   parseJWT,
   async (req: any, res: any) => {
-    console.log("User from review", req.user);
-    console.log(req.params.restaurantid);
-    const { rating, comment } = req.body;
-    const existingRestaurant = await pool.query(
-      "SELECT * FROM restaurant WHERE restaurantid=$1",
-      [req.params.restaurantid]
-    );
-    if (!existingRestaurant) {
-      return res.status(404).json({ message: "Restaurant Doesn't Exist" });
+    try {
+      console.log("User from review", req.user);
+      console.log(req.params.restaurantid);
+      const { rating, comment = "" } = req.body;
+      const existingRestaurant = await pool.query(
+        "SELECT * FROM restaurant WHERE restaurantid=$1",
+        [req.params.restaurantid]
+      );
+      if (!existingRestaurant) {
+        return res.status(404).json({ message: "Restaurant Doesn't Exist" });
+      }
+      console.log(
+        "Restaurant Det. from createReview",
+        existingRestaurant.rows[0]
+      );
+      const newReview = {
+        restaurant_id: req.params.restaurantid,
+        user_id: req.user.id,
+        rating: rating,
+        comment: comment,
+        created_at: new Date(),
+      };
+      const createdReview = await pool.query(
+        "INSERT INTO reviews(restaurant_id,user_id,rating,comment,created_at)VALUES($1,$2,$3,$4,$5) RETURNING *",
+        [
+          newReview.restaurant_id,
+          newReview.user_id,
+          newReview.rating,
+          newReview.comment,
+          newReview.created_at,
+        ]
+      );
+      if (!createdReview) {
+        return res
+          .status(401)
+          .json({ message: "Problem while creating the Review." });
+      }
+      console.log(createdReview.rows[0]);
+      return res.status(201).json(createdReview.rows[0]);
+    } catch (error) {
+      console.log(error);
     }
-    console.log(
-      "Restaurant Det. from createReview",
-      existingRestaurant.rows[0]
-    );
-    const newReview = {
-      restaurant_id: req.params.restaurantid,
-      user_id: req.user.id,
-      rating: rating,
-      comment: comment,
-      created_at: new Date(),
-    };
-    const createdReview = await pool.query(
-      "INSERT INTO reviews(restaurant_id,user_id,rating,comment,created_at)VALUES($1,$2,$3,$4,$5) RETURNING *",
-      [
-        newReview.restaurant_id,
-        newReview.user_id,
-        newReview.rating,
-        newReview.comment,
-        newReview.created_at,
-      ]
-    );
-    if (!createdReview) {
-      return res
-        .status(401)
-        .json({ message: "Problem while creating the Review." });
-    }
-    console.log(createdReview.rows[0]);
-    return res.status(201).json(createdReview.rows[0]);
   }
 );
 
 router.get("/:restaurantid/getAllReviews", async (req: any, res: any) => {
-  const allReviews = await pool.query(
-    "SELECT * FROM reviews WHERE restaurant_id=$1",
-    [req.params.restaurantid]
-  );
-  if (!allReviews) {
+  try {
+    const { page = 1, sortOption = "new" } = req.query;
+    const limit = 5;
+    const offset = (page - 1) * limit;
+
+    if (page == 2) console.log("scroll page num", page);
+
+    let orderByClause = "created_at DESC";
+    if (sortOption === "old") {
+      orderByClause = "created_at ASC";
+    }
+
+    const allReviews = await pool.query(
+      `SELECT * FROM reviews WHERE restaurant_id=$1 ORDER BY ${orderByClause} LIMIT $2 OFFSET $3`,
+      [req.params.restaurantid, limit, offset]
+    );
+    if (!allReviews) {
+      return res.json({
+        message: "No Reviews Availabe.",
+      });
+    }
+    for (const review of allReviews.rows) {
+      const userName = await pool.query(
+        "SELECT name FROM userinfo where id=$1",
+        [review.user_id]
+      );
+      review["username"] = userName.rows[0].name;
+    }
+    // console.log(allReviews.rows[0]);
+    const totalReviews = await pool.query(
+      "SELECT COUNT(*) AS total_reviews FROM reviews WHERE restaurant_id = $1;",
+      [req.params.restaurantid]
+    );
+    console.log("total", totalReviews.rows[0].total_reviews);
+
+    return res.status(201).json(allReviews.rows);
+  } catch (error) {
+    console.log(error);
     return res.json({
-      message: "Problem while Getting all the Reviews for this Restaurant.",
+      message: "Problem while Getting the Reviews for this Restaurant.",
     });
   }
-  console.log(allReviews.rows[0]);
-  return res.json(allReviews.rows);
 });
 module.exports = router;
 export {};
